c8b4e250b79ac40f1e6e1b80afef31331bdc906d
Nabil Ahmad Firdaus <nabilahmad544@gmail.com>
2025-01-24 12:56:15 +0700

    Praktikum Seeder dan Faker

---
+++ database/seeders/CourtTypeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use faker\Factory as Faker;
+
+class CourtTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create('id_ID');
+        for($i=1; $i <=10; $i++ ){
+            DB::table('court__types')->insert([
+                'name' => $faker->name
+            ]);
+        }
+    }
+}
